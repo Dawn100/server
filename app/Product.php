580282033bd67ea732d248ff3ea5a4476fd8d63f
PeterKahenya <peterkahenyanjoki@gmail.com>
2019-09-12 17:30:05 +0300

    Implemented relationships in the models

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // protected $fillable = [
+    //     'name', 'price','description','stock','category_id','photo','user_id'
+    // ];
+
+    // protected $hidden=[
+    //   'created_at','updated_at','user_id','category_id'
+    // ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
