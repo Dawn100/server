3a910191383c96c338af615eed22de2e1542f6d1
PeterKahenya <peterkahenyanjoki@gmail.com>
2019-09-12 14:59:38 +0300

    Added Products resource routes set

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 
 
 Route::resource('categories','CategoryController');
+Route::resource('products','ProductController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
