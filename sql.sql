dc4240785e20d67eaf63f0b36834d6e7b06437dd
PeterKahenya <peterkahenyanjoki@gmail.com>
2019-09-12 12:02:01 +0300

    Altered the password to solve unknown authentication error

--- sql.sql
+++ sql.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE esoko;
 CREATE USER puser@localhost IDENTIFIED WITH mysql_native_password BY '1@2@3#4';
 GRANT ALL ON esoko.* TO puser@localhost;
+ALTER USER 'puser'@'localhost' IDENTIFIED WITH mysql_native_password BY '1@2@3#4';
