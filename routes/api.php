198d69b35324fdf3794ab14c28f3702193df44f2
PeterKahenya <peterkahenyanjoki@gmail.com>
2019-09-12 17:40:18 +0300

    Added route for getting products for a given category

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 
 
-Route::resource('categories','CategoryController');
+Route::resource('categories','CategoryController')->middleware('cors');
+Route::get('categories/{id}/products', 'CategoryController@products')->middleware('cors');;
+
 Route::resource('products','ProductController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
