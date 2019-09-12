012d2cf289490c98b4adcc5fad48f15728730a89
PeterKahenya <peterkahenyanjoki@gmail.com>
2019-09-12 18:11:53 +0300

    Added protected routes for products

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Http\Request;
 
-
 Route::resource('categories','CategoryController')->middleware('cors');
-Route::get('categories/{id}/products', 'CategoryController@products')->middleware('cors');;
 
-Route::resource('products','ProductController');
+Route::get('categories/{id}/products', 'CategoryController@products')->middleware('cors');
+
+Route::resource('products','ProductController')->middleware('auth:api')->middleware('cors');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:api')->get('/user/products', function (Request $request) {
+    return $request->user()->products;
 });
