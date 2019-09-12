9fcba3ab14c1e4d8d87ef1ec23ab882757629b98
PeterKahenya <peterkahenyanjoki@gmail.com>
2019-09-12 12:33:21 +0300

    Added Category resource route set

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 
 
+Route::resource('categories','CategoryController');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
